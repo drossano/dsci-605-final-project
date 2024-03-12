4c869f97c3505030db493f52c951da4bf52c4be8
Dean <72749793+drossano@users.noreply.github.com>
2024-03-12 15:08:47 -0400

    Filter out HI and AK from unemployment rate and group unemployment rates by state and year

--- dean-rossano-final-proect.R
+++ dean-rossano-final-proect.R
@@ -14,13 +14,13 @@ library(gridExtra)       #grid.arrange()
 # setwd("~/cs-masters/2024/spring/dsci-605/dsci-605-final-project/data")
 
 # read the data
-unemploy_rate <- read.csv("data/unemployment_county.csv")
+unemploy_rate <- read_csv("data/unemployment_county.csv")
 
-crime_rate <- read.csv("data/crime_and_incarceration_by_state.csv")
+crime_rate <- read_csv("data/crime_and_incarceration_by_state.csv")
 
 states <- st_read("data/tl_2019_us_state/tl_2019_us_state.shp")
 
-# filter the data to the contiguous states
+##  filter the data to the contiguous states
 contiguous_states <- states%>% filter(STUSPS!="AK"& 
                                        STUSPS!="AS"& 
                                        STUSPS!="MP"& 
@@ -32,3 +32,20 @@ contiguous_states <- states%>% filter(STUSPS!="AK"&
 # check that the length is 49 ie; 49 states
 
 length(unique(contiguous_states$STUSPS))
+
+## process the unenmployment rate data
+
+# check if the amount of states is 49
+unique(unemploy_rate $State)
+length(unique(unemploy_rate $State))
+# returns 50
+
+# filter out HI and AK and group by year and state
+
+unemploy_rate <-  unemploy_rate %>% filter(State!="AK"& State!="HI") %>%
+  group_by(State,Year) %>% 
+   summarise(Totalforce=sum(`Labor Force`),
+             Totalemployed=sum(Employed),
+             Totalunemployed=sum(Unemployed),
+             Meanrate=mean(`Unemployment Rate`,rm.na=TRUE)
+  )
