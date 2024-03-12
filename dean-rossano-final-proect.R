2e721fb0cf42e7168064f37f8cfb6c695806770e
Dean <72749793+drossano@users.noreply.github.com>
2024-03-12 14:00:49 -0400

    Filter to the coniguous 49 states

--- dean-rossano-final-proect.R
+++ dean-rossano-final-proect.R
@@ -19,3 +19,16 @@ unemploy_rate <- read.csv("data/unemployment_county.csv")
 crime_rate <- read.csv("data/crime_and_incarceration_by_state.csv")
 
 states <- st_read("data/tl_2019_us_state/tl_2019_us_state.shp")
+
+# filter the data to the contiguous states
+contiguous_states <- states%>% filter(STUSPS!="AK"& 
+                                       STUSPS!="AS"& 
+                                       STUSPS!="MP"& 
+                                       STUSPS!="PR"& 
+                                       STUSPS!="VI"& 
+                                       STUSPS!="HI"& 
+                                       STUSPS!="GU")
+
+# check that the length is 49 ie; 49 states
+
+length(unique(contiguous_states$STUSPS))
